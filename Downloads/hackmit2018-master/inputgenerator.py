227c3253071a1d4d193dad802e82f5f64c86a905
Zoe Anderson <zoeand398@gmail.com>
2018-09-15 16:00:40 -0400

    ints and floats

---
+++ Downloads/hackmit2018-master/inputgenerator.py
@@ -0,0 +1,12 @@
+import random
+import sys
+
+def gen_int():
+    return random.randint(-(sys.maxsize), sys.maxsize)
+def gen_float():
+    #print(type(sys.maxsize * 1.0))
+    return random.randrange(-(sys.maxsize * 1.0), sys.maxsize * 1.0)
+
+if __name__ == '__main__':
+    print("exec")
+    print(gen_float())
